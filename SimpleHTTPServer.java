450461eb7942a3669540c58eeb6340ae5c6f4593
Min Su Kim <mskim3494@Mins-MacBook-Pro.local>
2018-11-29 23:59:01 -0600

    studying http

---
+++ SimpleHTTPServer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+	* Java program to create a simple HTTP Server to demonstrate how to use
+	* ServerSocket and Socket class.
+	* When you run the above program in Eclipse or from command line and connect to the http://localhost:8080
+**/
+
+public class SimpleHTTPServer {
+    public static void main(String args[]) throws IOException {
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+                System.out.println(socket);
+            }
+        }
+    }
+}
